1de519ed6262fea355efe417de0da2332b70d2ae
Luísa Cristina Ferreira <luisac.paim@hotmail.com>
2021-07-09 19:01:32 -0300

    movimento da cobrinha

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ snake [0] = {
 }
 
 /*Variável para que a cobrinha ande: */
-
+let direction = "right";
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -23,6 +23,29 @@ function criarCobrinha() {
     }
 }
 
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
 
-criarBG();
-criarCobrinha();
+    /* definindo ponto de partida da cobrinha, para quando jogo for resetado */
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop(); /*tira o ultimo elemento do array*/
+
+    /*cabeça móvel da cobrinha; " 'unshift' - acrescenta uma no primeiro elemento*/
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}   
+
+let jogo = setInterval (iniciarJogo, 100); /*100 milisegundos; intervalo para atualização do jogo(?) */
